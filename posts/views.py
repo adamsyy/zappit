714f6edc974b83c74656dd6b975f54b72dbae189
Adam Oommen Jacob <adamoommenjacob@gmail.com>
2021-08-25 22:06:30 +0530

    unique voting added

--- posts/views.py
+++ posts/views.py
@@ -1,9 +1,10 @@
 from django.contrib.auth.models import Permission
-from .serializers import PostSerializer
-from posts.models import Post
+from .serializers import PostSerializer, VoteSerializer
+from posts.models import Post,Vote
 from django.shortcuts import render
 from rest_framework import generics,permissions
-from .models import Post
+from .models import Post, Vote
+from rest_framework.exceptions import ValidationError
 # Create your views here.
 
 class PostList(generics.ListCreateAPIView):
@@ -13,5 +14,20 @@ class PostList(generics.ListCreateAPIView):
 
     def perform_create(self,serializer):
         serializer.save(poster=self.request.user)
+
+
+class VoteCreate(generics.CreateAPIView):
+    serializer_class=VoteSerializer
+    permission_classes=[permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        user=self.request.user
+        post=Post.objects.get(pk=self.kwargs['pk'])
+        return Vote.objects.filter(voter=user,post=post)
+
+    def perform_create(self,serializer):
+        if self.get_queryset().exists():
+            raise ValidationError('oru pravshyam madi')
+        serializer.save(voter=self.request.user,post=Post.objects.get(pk=self.kwargs['pk']))    
 
 
--- zappit/urls.py
+++ zappit/urls.py
@@ -20,4 +20,5 @@ from posts import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/posts',views.PostList.as_view()),
+    path('api/posts/<int:pk>/vote',views.VoteCreate.as_view()),
 ]
