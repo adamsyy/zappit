da094f5a123cc67ae2ba22638a4fdcb7cded5544
Adam Oommen Jacob <adamoommenjacob@gmail.com>
2021-08-26 09:35:16 +0530

    delete vote

--- posts/views.py
+++ posts/views.py
@@ -2,9 +2,10 @@ from django.contrib.auth.models import Permission
 from .serializers import PostSerializer, VoteSerializer
 from posts.models import Post,Vote
 from django.shortcuts import render
-from rest_framework import generics,permissions
+from rest_framework import generics,permissions,mixins,status
 from .models import Post, Vote
 from rest_framework.exceptions import ValidationError
+from rest_framework.response import Response
 # Create your views here.
 
 class PostList(generics.ListCreateAPIView):
@@ -16,7 +17,7 @@ class PostList(generics.ListCreateAPIView):
         serializer.save(poster=self.request.user)
 
 
-class VoteCreate(generics.CreateAPIView):
+class VoteCreate(generics.CreateAPIView,mixins.DestroyModelMixin):
     serializer_class=VoteSerializer
     permission_classes=[permissions.IsAuthenticated]
 
@@ -31,3 +32,9 @@ class VoteCreate(generics.CreateAPIView):
         serializer.save(voter=self.request.user,post=Post.objects.get(pk=self.kwargs['pk']))    
 
 
+    def delete(self,request,*args,**kwargs):
+        if self.get_queryset().exists():
+            self.get_queryset().delete()
+            return Response(status=status.HTTP_204_NO_CONTENT)
+        else:
+            raise ValidationError('vote cheydila mahn!')
